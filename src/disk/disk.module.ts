bf3c48b1bcf0f0bdf55524ea5f20df5b62ce2530
Yan P Aung <yan.pye.aung@gmail.com>
2021-11-14 13:30:37 -0800

    Wire up all the dependencies

--- src/disk/disk.module.ts
+++ src/disk/disk.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DiskService } from './disk.service';
+import { PowerModule } from '../power/power.module';
+
 
 @Module({
-  providers: [DiskService]
+  imports: [PowerModule],
+  providers: [DiskService],
+  exports: [DiskService],
 })
-export class DiskModule {}
+export class DiskModule { }
--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from '../power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) { }
+
+  getData() {
+    console.log('Drawring 20 wats of power from PowerService');
+    this.powerService.supplyPower(20);
+    return 'data!';
+  }
+}
